63aa1e1ddef04f294d3d86ff265d180dd54a5ed7
Riyara Master <rtmaster@udel.edu>
2021-09-30 22:44:03 -0400

    Updates

---
+++ precache-manifest.d0850109973fe209e11fd4dabc8338af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bab80c684331e17b84bfb282e691e51d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae3fe406f6f2226c4bcd",
+    "url": "/static/css/main.90c3d31e.chunk.css"
+  },
+  {
+    "revision": "de1ba450db8ee01d5f3e",
+    "url": "/static/js/2.f16758bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f16758bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae3fe406f6f2226c4bcd",
+    "url": "/static/js/main.2d775c2c.chunk.js"
+  },
+  {
+    "revision": "69bbd7c1381a265f29c9",
+    "url": "/static/js/runtime-main.fda65720.js"
+  }
+]);
